87414f32285697831edbec161ae7c784f403b91d
Angelo Padron <padron891@gmail.com>
2024-06-14 13:21:22 -0300

    Add changes: - Spaceship scene - Changes on audio - Remove unused assets - Other minor changes

--- src/entities/utils/planting_zone.gd
+++ src/entities/utils/planting_zone.gd
@@ -7,15 +7,6 @@ extends StaticBody2D
 var plant_is_growing = false
 var collectable = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_grow_timer_timeout():
 	plant.play("grow")
@@ -30,11 +21,9 @@ func _on_area_2d_area_entered(area):
 func _on_plant_animation_finished():
 	collectable = true
 	plant_is_growing = false
-	print_debug("planting_zone: plan finished grow")
 
 
 func _on_area_2d_body_entered(player: Player):
-	print_debug("planting_zone: player entered growing area")
 	if collectable:
 		audio_player.play()
 		player.add_crop()
